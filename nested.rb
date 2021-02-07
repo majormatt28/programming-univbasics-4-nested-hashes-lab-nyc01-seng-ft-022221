df7e3e5f0926e479bdd94955792120a5f2dfa48c
Mathew E Newton <elijah2895@hotmail.com>
2021-02-07 17:21:16 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -107,8 +107,8 @@ def changing_alan
         :languages => ["C"]
       }
     }
-programmer_hash[:alan_kay][:known_for] = "alans_new_info"
-programmer_hash
+programmer_hash[:alan_kay][:known_for] = "GUI"
+puts programmer_hash
 end
 
 def adding_to_dennis
